e58dac3fcb7a8dca7eb41f387e17fd404d8935d5
N451M4K <khannasimahmad@gmail.com>
2023-02-09 13:53:06 +0530

    completed edit functionality

--- client/src/EditModal.js
+++ client/src/EditModal.js
@@ -1,23 +1,52 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import Message from './Message';
 const EditModal = (props) => {
-    
+
     let { currentRow } = props;
     console.log('procurrentRowps', currentRow.productName);
-    // const [category, setCategory] = useState([]);
+    const [category, setCategory] = useState([]);
     const [productName, setProductName] = useState('');
     const [productDescription, setProductDescription] = useState('')
-    // const [categoryId, setCategoryId] = useState();
-    
-    
-    
+    const [categoryId, setCategoryId] = useState();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isError, setIsError] = useState(true);
+
+
     useEffect(() => {
         setProductName(currentRow.productName);
         setProductDescription(currentRow.productDescription)
         console.log('procurrentRowps', currentRow.productName);
+        fetchCategoryList();
     }, [currentRow.productName, currentRow.productDescription])
-    async function handleSubmit(){
 
+    async function fetchCategoryList() {
+        let response = await fetch('http://localhost:5000/myapp/category/category_list');
+        let categoryList = await response.json();
+        setCategory(categoryList.data)
+    }
+
+    async function handleSubmit() {
+        try {
+        let response = await fetch(`http://localhost:5000/myapp/product/update_product?productId=${currentRow.productId}`, {
+            method: 'PUT', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ "productName": productName, "productDescription": productDescription, "categoryId": categoryId }),
+        });
+        let updated = await response.json();
+            if (updated.success === true) {
+                setErrorMessage("Form submitted successfully");
+                setIsError(false);
+            } else {
+                setErrorMessage(updated.msg)
+                setIsError(true);
+            }
+        } catch (err) {
+            setErrorMessage(err.message)
+            setIsError(true);
+        }
     }
     return (
         <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -31,23 +60,27 @@ const EditModal = (props) => {
                     </div>
                     <div className="modal-body">
                         <form className='form ' onSubmit={handleSubmit}>
+                            
                             <input type="text" className="form-control" value={productName} placeholder='Product Name' onChange={(event) => setProductName(event.target.value)} required />
+                            
                             <input type="text" className='form-control' value={productDescription} placeholder='Product Description' onChange={(event) => setProductDescription(event.target.value)} required />
-                            {/* <select className='form-control' value={categoryId} onChange={(event) => setCategoryId(event.target.value)} required>
-                                <option defaultValue={''}>Select Category</option>
+                            
+                            <select className='form-control' value={categoryId} onChange={(event) => setCategoryId(event.target.value)} required>
+                                <option defaultValue={currentRow.categoryId}>{currentRow.categoryName}</option>
                                 {category.map((element) => {
                                     return <option value={element.id} key={element.id}>{element.categoryName}</option>
                                 })}
-                            </select> */}
-                            {/* <button type="submit" className="btn btn-primary form-control">Submit</button> */}
+                            </select>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" className="btn btn-primary">Save changes</button>
+                            </div>
                         </form>
                     </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" className="btn btn-primary">Save changes</button>
-                    </div>
+
                 </div>
             </div>
+            <Message message={errorMessage} isError={ isError} />
         </div>
     )
 }
